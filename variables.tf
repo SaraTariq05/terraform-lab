de3a4c241ede989ae9c781967459d3bafc151328
Sara <tariqsara995@gmail.com>
2025-12-22 11:51:33 +0500

    Dockerization

--- variables.tf
+++ variables.tf
@@ -1,7 +1,7 @@
 variable "bucket_name" {
   description = "Name of the S3 bucket"
   type        = string
-  default     = "sara-static-site-terraform-2025-v2"
+  default     = "sara-static-site-terraform-2025-docker"
 }
 
 variable "aws_region" {
